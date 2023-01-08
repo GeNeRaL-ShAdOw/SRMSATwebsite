fbac450041b0bf6fac13358e0d2ea725744b99c6
GeNeRaL-ShAdOw <chinmayapurohit23@gmail.com>
2023-01-08 10:43:42 +0530

    minor changes in CSS and added button w/o functionality

--- srmsatwebsite/src/Components/whatwedo.jsx
+++ srmsatwebsite/src/Components/whatwedo.jsx
@@ -10,6 +10,7 @@ function Whatwedo()
                 <font size='+2' className="WWD-box-content">SRMSAT WAS FOUNDED IN 2009, WITH A SMAILL TEAM OF 59 STUDENTS AND ONE AMBITION - TO SEND A SATELLITE TO SPACE.</font>
                 <font size='+2' className="WWD-box-content">THIS IS REALISED IN 2011, WHEN THE FIRST SRM INDEGENOUSLY BUILT NANOSATELLITE, SRMSAT 1 WAS LAUNCHED INTO SPACE.</font>
                 <font size='+2' className="WWD-box-content">SRMSAT REQUIRES THE BEST AMONG THE BEST, THIS IS LITERALLY ROCKET SCIENCE. THINK YOU HAVE WHAT IT TAKES?</font>
+                <button><font size='+1'>SIGN ME UP!</font></button>
             </div>
             <div className='borderline-mssn'>
                 <font size='+4' className="mssn-box-title">MISSIONS</font>
