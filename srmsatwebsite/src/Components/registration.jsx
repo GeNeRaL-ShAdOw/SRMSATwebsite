d98164d6a71b65be7197507af26d49137f9af6d3
GeNeRaL-ShAdOw <chinmayapurohit23@gmail.com>
2023-01-09 18:55:42 +0530

    Added some changes to registration form

--- srmsatwebsite/src/Components/registration.jsx
+++ srmsatwebsite/src/Components/registration.jsx
@@ -1,13 +1,44 @@
+import React, { useState } from 'react';
+
 import './registration.css'
 
 function Registration()
 {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (name === '' || email === '') {
+          setError('NAME AND EMAIL ARE REQUIRED!');
+          return;
+        }
+        // Perform API call or other logic here
+      };
     return (
-    <div className="App">
+    <div className="App2">
         <font size='+8' className='font-face-er-main-text-register'>R E G I S T R A T I O N</font>
         <font size='+2' className='font-face-er-smol-text-join'>J O I N I N G</font>
         <font size='+2' className='font-face-er-smol-text-space'>T H E</font>
         <font size='+2' className='font-face-er-smol-text-space'>F O R C E</font>
+        <form onSubmit={handleSubmit}>
+            {error && <p><font size='+1'>{error}</font></p>}
+            <label >
+                <font size='+3'>NAME:</font>
+                <input type="text" name="name" value={name} onChange={(event) => setName(event.target.value)} />
+            </label>
+            <br/>
+            <br/>
+            <br/>
+            <label>
+                <font size='+3'>EMAIL:</font>
+                <input type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+            </label>
+            <br/>
+            <br/>
+            <br/>
+            <button className='button1' type="submit"><font size='+2'>SUBMIT</font></button>
+        </form>
     </div>)
 }
 
--- srmsatwebsite/src/Components/whatwedo.jsx
+++ srmsatwebsite/src/Components/whatwedo.jsx
@@ -14,7 +14,9 @@ function Whatwedo()
                 <font size='+2' className="WWD-box-content">SRMSAT WAS FOUNDED IN 2009, WITH A SMAILL TEAM OF 59 STUDENTS AND ONE AMBITION - TO SEND A SATELLITE TO SPACE.</font>
                 <font size='+2' className="WWD-box-content">THIS IS REALISED IN 2011, WHEN THE FIRST SRM INDEGENOUSLY BUILT NANOSATELLITE, SRMSAT 1 WAS LAUNCHED INTO SPACE.</font>
                 <font size='+2' className="WWD-box-content">SRMSAT REQUIRES THE BEST AMONG THE BEST, THIS IS LITERALLY ROCKET SCIENCE. THINK YOU HAVE WHAT IT TAKES?</font>
-                <button className='button1' onClick={gotoregistration}><font size='+1'>SIGN ME UP!</font></button>
+                <div className='Strtbutton'>
+                    <button className='button1' onClick={gotoregistration}><font size='+1'>SIGN ME UP!</font></button>
+                </div>
             </div>
     );
 }
